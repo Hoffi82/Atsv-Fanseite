131256c510620d9a6b785ad5b6a0c977fa9a6dce
Hoffi82 <5yggzvn2rs@privaterelay.appleid.com>
2026-08-23 19:44:14 +0200

    Formkurve um Spieltag 3 erweitern

--- js/form-data.js
+++ js/form-data.js
@@ -9,7 +9,8 @@ window.ATSV_FORM_GAMES = [
   { date: '2026-07-24', type: 'Testspiel', home: 'ATSV Erlangen U23 2', away: 'ATSV Forchheim', homeScore: 0, awayScore: 3 },
   { date: '2026-08-02', type: 'Testspiel', home: 'ATSV Forchheim', away: 'BSC Erlangen', homeScore: 2, awayScore: 5 },
   { date: '2026-08-09', type: 'Liga', home: 'ATSV Forchheim', away: 'DJK Hallerndorf', homeScore: 2, awayScore: 4 },
-  { date: '2026-08-15', type: 'Liga', home: 'ASV Niederndorf', away: 'ATSV Forchheim', homeScore: 3, awayScore: 0 }
+  { date: '2026-08-15', type: 'Liga', home: 'ASV Niederndorf', away: 'ATSV Forchheim', homeScore: 3, awayScore: 0 },
+  { date: '2026-08-23', type: 'Liga', home: 'ATSV Forchheim', away: 'DJK Erlangen II', homeScore: 2, awayScore: 0 }
 ];
 
 window.ATSV_FORM_CONFIG = {
